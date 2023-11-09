50217d663697b8f339c97010711cb1c76fbdc113
sraddha1208 <85079404+sraddha1208@users.noreply.github.com>
2023-11-09 15:01:02 +0530

    Update Wetland_Monitoring.sol

--- Wetland_Monitoring.sol
+++ Wetland_Monitoring.sol
@@ -1,1 +1,75 @@
+pragma solidity ^0.8.0;
+
+contract wetland_monitoring
+{
+    struct monitoringparameters
+    {
+        uint256 pH;
+        uint256 DO;
+        uint256 temp;
+        uint256 humidity;
+        uint256 SM;
+        uint256 tds;
+        uint256 turb;
+    }
+    struct thresholds
+    {
+        uint256 pHthreshold;
+         uint256 DOthreshold;
+          uint256 tempthreshold;
+           uint256 humiditythreshold;
+            uint256 SMthreshold;
+             uint256 tdsthreshold;
+              uint256 turbthreshold;
+    }
+    thresholds public Threshold;
+    mapping(address => bool) public eligibleForRewards;
+    address public ethers;
+    uint256 public reward;
+    event ObservationRewarded(address indexed user, uint256 reward);
+    constructor(uint256 _RewardInEther) 
+    {
+        reward = _RewardInEther * 1 ether; // Convert to wei
+    }
+      struct userobservation
+      {
+        string userobs;
+        bool isvalid;
+      }
+      mapping(address => userobservation) public userobs;
+    function submitObservation(string memory observation) public
+    {
+        address owner;
+        owner = msg.sender;
+        require(msg.sender != owner, "Owner cannot submit observations.");
+    }
+    function validateobservation(bool isaccurate) public 
+    {
+        uint256 pH;
+        uint256 DO;
+        uint256 temp;
+        uint256 humidity;
+        uint256 SM;
+        uint256 tds;
+        uint256 turb;
+        if (
+            pH <= Threshold.pHthreshold ||
+            turb <= Threshold.turbthreshold ||
+            tds <= Threshold.tdsthreshold || 
+            DO<= Threshold.DOthreshold ||
+            SM<= Threshold.SMthreshold
+        ) {
+            eligibleForRewards[msg.sender] = true;
+            emit ObservationRewarded(msg.sender, reward);
+        }
+    }
+    function claimReward() external 
+    {
+        require(eligibleForRewards[msg.sender], "User is not eligible for rewards");
+        // Transfer rewards to the user
+     payable(msg.sender).transfer(reward);
+
+        eligibleForRewards[msg.sender] = false; // Mark the user as claimed
+    }
+    }
 
