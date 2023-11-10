447a64dd13a859d5b979d80af61fb41c3df3bbc3
sraddha1208 <85079404+sraddha1208@users.noreply.github.com>
2023-11-10 16:47:42 +0530

    Update Sensordatasecurity.sol

--- Sensordatasecurity.sol
+++ Sensordatasecurity.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.8.0;
 
 contract SensorDataIntegrity{
    address public owner;
-   mapping(address => bool) public authorizedSensors;
-   mapping(address => bytes32) sensorDataId;
+   mapping(address => bool) public authorizedSensors;//indicates whether the sensor is authorized or not by checking the address
+   mapping(address => bytes32) sensorDataId;//Maps the sensor data Id to 32 byte address 
    mapping(address => bytes) encryptedSensorData;
 
     event SensorAuthorized(address indexed sensor);
@@ -18,6 +18,7 @@ contract SensorDataIntegrity{
         require(msg.sender == owner, "Only the owner can authorize sensors.");
         _;
     }
+//Function to authorize the sensor
      function authorizeSensor(address sensorAddress) external onlyOwner 
      {
         require(sensorAddress != address(0), "Invalid sensor address.");
@@ -26,10 +27,12 @@ contract SensorDataIntegrity{
         authorizedSensors[sensorAddress] = true;
         emit SensorAuthorized(sensorAddress);
     }
+//Function to validate whether a sensor is authorized or not
     function sensorvalidation(address sensorAddress) external view returns (bool) 
     {
         return authorizedSensors[sensorAddress];
     }
+//Function to store the data recieved from the sensors
      function storeSensorData(bytes32 dataId, bytes memory encryptedData) public 
      {
         sensorDataId[msg.sender] = dataId;
